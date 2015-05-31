a3ef821735783acd93c7834f5963b657e388de4e
Alex Naish <lexi.drage@gmail.com>
2015-05-31 20:59:26 +0100

    tweak config

---
+++ config/production.js
@@ -0,0 +1,15 @@
+module.exports = {
+    mongo: {
+        host: "127.3.100.130:27017",
+        db: "jenkings",
+        user: "admin",
+        pass: "hVdCnwfBTr-i"
+    },
+    pusher: {
+        appId: "122412",
+        key: "488fc1c2041ce7cfeac8",
+        secret: "00527929f69bb53685fc",
+        channel: "jenkins",
+        event: "job-run"
+    }
+};
