60899e0d5c23f41adef5fa6f23421b23dbebcccf
Alex Naish <lexi.drage@gmail.com>
2015-05-31 20:22:14 +0100

    remove scheduler

--- api/scheduler/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  
-    jobs: ['iceberg-frontend-protractor', 'iceberg-frontend-unit', 'iceberg-backend-e2e', 'iceberg-backend-unit'],
-    jenkinsHost: 'http://build.nowtv.bskyb.com/view/6.%20Iceberg/job/'
-    
-};
