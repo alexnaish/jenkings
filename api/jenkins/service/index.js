ae2c2a8aea07b43e46e5c0eed1bb21d6ebe14ac3
Alex Naish <alex.naish@bskyb.com>
2015-06-15 15:02:06 +0100

    moar logging

--- api/jenkins/service/index.js
+++ api/jenkins/service/index.js
@@ -25,6 +25,9 @@ module.exports = {
 
         JobRun.findOne(queryObject, function (err, result) {
             if (result) {
+
+                console.log('querying:::::', generateJenkinsJobApiUrl(job, build));
+
                 request.get(generateJenkinsJobApiUrl(job, build), function (error, response, body) {
                     if (!error && response && response.statusCode === 200) {
                         try {
@@ -32,6 +35,8 @@ module.exports = {
                             var payload = _.pick(bodyJson, ['result', 'builtOn', 'duration', 'culprits']);
                             payload.node = payload.builtOn;
                             delete payload.builtOn;
+
+                            console.log('Jenkings result::::::', payload);
 
                             JobService.update(queryObject, payload, function (status, response) {
                                 if (status === 200) {
