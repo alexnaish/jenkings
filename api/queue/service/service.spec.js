907e418fb949aa7787f0ec1d80e473799e8a8c02
Alex Naish <lexi.drage@gmail.com>
2015-06-19 18:04:02 +0100

    add QueueService spec file

---
+++ api/queue/service/service.spec.js
@@ -0,0 +1,17 @@
+var QueueService = require('./'),
+    sinon = require('sinon'),
+    expect = require('chai').expect;
+
+describe('QueueService', function () {
+
+    beforeEach(function () {});
+
+    afterEach(function () {});
+
+    it('should apply the function specified with the parameters after the interval defined', function () {
+
+        expect(true).to.be.equal(true);
+
+    });
+
+});
