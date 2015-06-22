b988fcc9e7c55d9da67071036ac8555a709bec06
Iceberg <DL-NowTV-IcebergTeam@bskyb.com>
2015-06-22 16:14:05 +0100

    fix typo

--- api/stats/service/index.js
+++ api/stats/service/index.js
@@ -16,55 +16,54 @@ module.exports = {
         JobRun.aggregate([
                 {
                     $match: matchObject
-                }
             }, {
-                $project: {
-                    dateCreated: 1,
-                    year: {
-                        $year: "$dateCreated"
-                    },
-                    month: {
-                        $month: "$dateCreated"
-                    },
-                    day: {
-                        $dayOfMonth: "$dateCreated"
-                    },
-                    jobName: 1,
-                    result: 1
-                }
-            }, {
-                $project: {
-                    date: {
-                        $concat: [{
-                            "$substr": ["$year", 0, 4]
-                        }, ":", {
-                            "$substr": ["$month", 0, 2]
-                        }, ":", {
-                            "$substr": ["$day", 0, 2]
-                        }]
-                    },
-                    jobName: 1,
-                    result: 1
-                }
-            }, {
-                $group: {
-                    _id: {
-                        "date": "$date",
-                        "jobName": "$jobName",
-                        "result": "$result"
-                    },
-                    count: {
-                        $sum: 1
+                    $project: {
+                        dateCreated: 1,
+                        year: {
+                            $year: "$dateCreated"
+                        },
+                        month: {
+                            $month: "$dateCreated"
+                        },
+                        day: {
+                            $dayOfMonth: "$dateCreated"
+                        },
+                        jobName: 1,
+                        result: 1
                     }
-                }
             }, {
-                $project: {
-                    _id: 0,
-                    date: "$_id.date",
-                    jobName: "$_id.jobName",
-                    result: "$_id.result",
-                    count: "$count"
-                }
+                    $project: {
+                        date: {
+                            $concat: [{
+                                "$substr": ["$year", 0, 4]
+                        }, ":", {
+                                "$substr": ["$month", 0, 2]
+                        }, ":", {
+                                "$substr": ["$day", 0, 2]
+                        }]
+                        },
+                        jobName: 1,
+                        result: 1
+                    }
+            }, {
+                    $group: {
+                        _id: {
+                            "date": "$date",
+                            "jobName": "$jobName",
+                            "result": "$result"
+                        },
+                        count: {
+                            $sum: 1
+                        }
+                    }
+            }, {
+                    $project: {
+                        _id: 0,
+                        date: "$_id.date",
+                        jobName: "$_id.jobName",
+                        result: "$_id.result",
+                        count: "$count"
+                    }
             }],
             function (err, result) {
                 if (err) {
@@ -73,6 +72,6 @@ module.exports = {
                     callback(200, result);
                 }
             });
-}
+    }
 
 };
