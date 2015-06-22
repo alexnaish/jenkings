553a333b9160824ef0bace87e851ef8186df8ed3
Iceberg <DL-NowTV-IcebergTeam@bskyb.com>
2015-06-22 15:26:48 +0100

    tweak job result stats function

--- api/stats/service/index.js
+++ api/stats/service/index.js
@@ -7,19 +7,58 @@ module.exports = {
         JobRun.aggregate([
             {
                 $match: {
-                    branch: branch,
+                    branch: 'master',
                     result: {
                         $ne: 'PENDING'
                     }
                 }
             }, {
+                $project: {
+                    dateCreated: 1,
+                    year: {
+                        $year: "$dateCreated"
+                    },
+                    month: {
+                        $month: "$dateCreated"
+                    },
+                    day: {
+                        $dayOfMonth: "$dateCreated"
+                    },
+                    jobName: 1,
+                    result: 1
+                }
+            }, {
+                $project: {
+                    date: {
+                        $concat: [{
+                            "$substr": ["$year", 0, 4]
+                        }, ":", {
+                            "$substr": ["$month", 0, 2]
+                        }, ":", {
+                            "$substr": ["$day", 0, 2]
+                        }]
+                    },
+                    jobName: 1,
+                    result: 1
+                }
+            }, {
                 $group: {
                     _id: {
-                        $dayOfYear: "$dateCreated"
+                        "date": "$date",
+                        "jobName": "$jobName",
+                        "result": "$result"
                     },
-                    result: {
+                    count: {
                         $sum: 1
                     }
+                }
+            }, {
+                $project: {
+                    _id: 0,
+                    date: "$_id.date",
+                    jobName: "$_id.jobName",
+                    result: "$_id.result",
+                    count: "$count"
                 }
             }
     ], function (err, result) {
