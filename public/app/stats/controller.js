1abb1015b41eb2351f181ff02792879a1c08c203
Iceberg <DL-NowTV-IcebergTeam@bskyb.com>
2015-06-22 16:46:41 +0100

    get something on screen

--- public/app/stats/controller.js
+++ public/app/stats/controller.js
@@ -3,12 +3,12 @@ var component = angular.module('stats.controller', ['chart.js']);
 component.controller("StatsController", ['$scope', '$routeParams', 'StatsService', 'config', function ($scope, $routeParams, StatsService, config) {
     console.log('loaded StatsController');
 
-    $scope.trackingBranch = $routeParams.branchName || 'master';
+    $scope.trackingBranch = $routeParams.branchName;
 
     StatsService.getHistoricalStats($scope.trackingBranch).then(function (data) {
-        console.log('received:::', data);
+        $scope.data = data;
     });
 
-    console.log('config', config);
+    $scope.jobs = config.ci.jobs;
 
 }]);
