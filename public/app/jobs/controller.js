29914f645fe4782eb49291c19f3687d61c0fdec4
Iceberg <DL-NowTV-IcebergTeam@bskyb.com>
2015-06-26 13:54:06 +0100

    add logging into job template controller

--- public/app/jobs/controller.js
+++ public/app/jobs/controller.js
@@ -3,7 +3,11 @@ var component = angular.module('jobs.controller', []);
 component.controller("JobsController", ['$scope', 'JobService', '$routeParams', function ($scope, JobService, $routeParams) {
     console.log('loaded JobsController');
 
+    $scope.jobName = $routeParams.jobName;
+    $scope.buildId = $routeParams.buildId;
+
     JobService.listSpecificJob($routeParams.jobName, $routeParams.buildId).then(function (data) {
+        console.log('my data', data);
         $scope.job = data;
     });
 }]);
