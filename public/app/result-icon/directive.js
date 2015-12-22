08e370e1722835f9bf57e3b4cfce7701b1c4da68
Alex Naish <piotr.seefeld@bskyb.com>
2015-12-22 11:33:33 +0000

    @alexnaish add result icon feature, tweak panel styles

---
+++ public/app/result-icon/directive.js
@@ -0,0 +1,42 @@
+(function (component) {
+    
+    component.directive('resultIcon', function () {
+        return {
+            restrict: 'E',
+            controller: 'ResultIconController',
+            bindToController: true,
+            controllerAs: 'ctrl',
+            scope: {
+                result: '='
+            },
+            templateUrl: 'app/result-icon/template.html'
+        };
+    });
+
+    component.controller('ResultIconController', ['$scope', function ($scope) {
+
+        var colourMap = {
+            success: 'green',
+            failure: 'red',
+            unstable: 'red',
+            pending: 'amber',
+            aborted: 'grey'
+        };
+        
+        var iconMap = {
+            success: 'fa-check',
+            failure: 'fa-times',
+            unstable: 'fa-times',
+            pending: 'fa-cog fa-spin',
+            aborted: 'fa-exclamation'
+        };
+        
+        var result = this.result.toLowerCase();
+        
+        this.colour = colourMap[result] || 'red';
+        this.icon = iconMap[result] || 'fa-times';
+        this.title = this.result;
+
+    }]);
+
+})(angular.module('result-icon', []));
