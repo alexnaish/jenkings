8fbbb424a2d08e02f5d63566a6abedb511e40a4c
Alex Naish <lexi.drage@gmail.com>
2015-12-23 16:37:01 +0000

    @alexnaish fix broken spec file

--- api/jobs/api/api.spec.js
+++ api/jobs/api/api.spec.js
@@ -200,8 +200,11 @@ describe('JobRun API', function () {
 
 
                     expect(queueStub.secondCall.args[0]).to.be.equal('pending-job');
-                    expect(queueStub.secondCall.args[1][0]).to.be.equal('test');
-                    expect(queueStub.secondCall.args[1][1]).to.be.equal('2');
+
+                    var queueFunctionArg = queueStub.secondCall.args[1][0].toString();
+                    var objectIdRegex = /^[0-9a-fA-F]{24}$/;
+                    //force the regex result to a boolean...
+                    expect(!!queueFunctionArg.match(objectIdRegex)).to.be.equal(true);
 
                     expect(res.body).to.be.have.property('_id');
                     expect(res.body).to.be.have.property('jobName');
