c35aa2ca12621f66656a409f7feb6f6f5b60324e
Iceberg <iceberg@sky.uk>
2016-01-08 13:16:10 +0000

    @alexnaish remove unused next parameter

--- api/jenkins/api/index.js
+++ api/jenkins/api/index.js
@@ -2,13 +2,13 @@ var JenkinsService = require('../service/');
 
 module.exports = {
 
-    fetchJobRunInfo: function (req, res, next) {
+    fetchJobRunInfo: function (req, res) {
         JenkinsService.fetchAndPopulateJobRun(req.params.id, function (status, body) {
             res.status(status).json(body);
         });
     },
 
-    fetchJobRunTestReport: function (req, res, next) {
+    fetchJobRunTestReport: function (req, res) {
         JenkinsService.fetchAndReturnTestReport(req.params.id, function (status, body) {
             res.status(status).json(body);
         });
