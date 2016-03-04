b6c209ff1712635c0f434dc54adc820b21e9f636
Iceberg <dl-nowtv-icebergteam@bskyb.com>
2016-03-04 15:05:22 +0000

    @alexnaish move list filtering into the controller

--- public/app/latest/controller.js
+++ public/app/latest/controller.js
@@ -1,24 +1,33 @@
 (function (component) {
 
-    component.controller('LatestController', ['$scope', 'jobs', 'SocketIO', function ($scope, jobs, SocketIO) {
-        $scope.jobruns = jobs.slice(0, 20);
+    component.controller('LatestController', ['$scope', '$filter', 'jobs', 'SocketIO', function ($scope, $filter, jobs, SocketIO) {
+
+        $scope.jobruns = generateJobList(jobs);
 
         SocketIO.bindTo($scope);
 
         SocketIO.on('jenkings:new-job', function (data) {
             console.log('new job received', data.jobName, data.buildId, data.result);
             $scope.jobruns.push(data);
+            $scope.jobruns = generateJobList($scope.jobruns)
         });
 
         SocketIO.on('jenkings:job-updated', function (data) {
             console.log('job update received', data.jobName, data.buildId, data.result);
             for (var i = 0; i < $scope.jobruns.length; i++) {
                 if ($scope.jobruns[i].jobName === data.jobName && $scope.jobruns[i].buildId === data.buildId) {
+                  console.log('found a match!', $scope.jobruns[i].jobName, $scope.jobruns[i].buildId);
                     $scope.jobruns[i] = data;
                     return;
                 }
             }
         });
+
+        function generateJobList(originalJobs) {
+            var jobsToList = $filter('orderBy')(originalJobs, '-dateCreated');
+            return $filter('limitTo')(jobsToList, 20);
+        }
+
     }]);
 
 })(angular.module('latest.controller', ['jobs']));
